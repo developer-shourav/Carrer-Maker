051ec5a30c93f29b75aa1e7ee710be8c84661a56
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-10 12:34:14 +0600

    tailwind and daysi ui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import React from "react";
 
-
-function App() {
-
+const App = () => {
   return (
-    <div >
-    
-    </div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold text-red-900">Hello world!</h1>
 
-export default App
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  );
+};
+
+export default App;
