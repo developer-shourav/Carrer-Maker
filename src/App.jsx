f88ab4c665b10c9e42a81c97b3f879469d0ac287
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-10 13:20:02 +0600

    primary navbar complite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-red-900">Hello world!</h1>
-
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
+      <Navbar> </Navbar>
     </div>
   );
 };
