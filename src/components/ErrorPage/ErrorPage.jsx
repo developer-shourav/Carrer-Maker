4ab28138ee7aaa64cb4dfcf4c4ff72caf908eff6
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-11 19:39:34 +0600

    fancy erro route added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import ErrorImg from '../../assets/errorImg/404.gif';
 
 
 
@@ -8,12 +9,11 @@ const ErrorPage = () => {
     const error = useRouteError();
     console.error(error);
     return (
-        <div >
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.message}</i>
-        </p>
+        <div className='container text-center'>
+         <img className='text-center inline-block w-40 md:w-auto my-5' src={ErrorImg} alt="" />
+        <h1 className='text-2xl md:text-3xl font-bold text-red-700'><i>Page {error.statusText || error.message}</i></h1>
+        <p className='text-red-700'>{error.data}</p>
+        <button className='btn-primary my-5'><Link to='/'>Back To Home</Link></button>
       </div>
     );
 };
