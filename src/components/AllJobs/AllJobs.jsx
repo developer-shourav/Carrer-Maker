5bba7e48e177f83f31870af9ab8df49267a4eee5
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-11 12:53:51 +0600

    dynamic jobs added

--- src/components/AllJobs/AllJobs.jsx
+++ src/components/AllJobs/AllJobs.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-import LocationIcon from "../../assets/icons/job/location.png";
-import MoneyIcon from "../../assets/icons/job/money.png";
+import React, { useEffect, useState } from "react";
+import SingleJob from "../SingleJob/SingleJob";
+
 const AllJobs = () => {
+
+    const [allJobs, setAllJobs] = useState([]);
+     
+    useEffect( () => {
+        fetch('allJobs.json')
+        .then( res => res.json())
+        .then( data => setAllJobs(data))
+    },[])
+
   return (
     <div>
       <h1 className="text-4xl font-bold text-center">Featured Jobs</h1>
@@ -9,35 +18,14 @@ const AllJobs = () => {
         Top jobs for you that can change your life. Grew up you career here.
       </p>
 
+      {/*---- Individual Job ---- */}
       <div className="grid pt-5 gap-4 grid-cols-2 ">
-        <div className="border p-6 rounded-lg">
-          <img
-            className="mb-5"
-            src="https://i.ibb.co/Ny3QVCh/google.png"
-            alt=""
-          />
-          <h3 className="text-xl text-gray-700 font-bold">
-            Technical Database Engineer
-          </h3>
-          <p className="text-gray-500 font-bold mt-1 mb-3">Google LLC</p>
-          <button className="btn-custom me-3">Remote</button>{" "}
-          <button className="btn-custom">Full Time</button>
-          <div className="mt-4 mb-5 text-gray-600">
-            <span className="me-5">
-              {" "}
-              <img className="inline" src={LocationIcon} alt="" /> Dhaka,
-              Bangladesh
-            </span>
-
-            <span>
-              {" "}
-              <img className="inline" src={MoneyIcon} alt="" /> Salary : 100K -
-              150K
-            </span>
-          </div>
-          <button className="btn-primary mb-3">View Details</button>
-        </div>
+        {
+            allJobs?.map( job => <SingleJob key={job?.id} jobInfo={job}> </SingleJob>)
+        }
       </div>
+      
+     
     </div>
   );
 };
---
+++ src/components/SingleJob/SingleJob.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import LocationIcon from "../../assets/icons/job/location.png";
+import MoneyIcon from "../../assets/icons/job/money.png";
+const SingleJob = ({jobInfo}) => {
+    console.log(jobInfo);
+    const {companyLogo, jobTitle, companyName, jobType, jobDuration, location, salary} = jobInfo;
+  return (
+      <div className="border p-6 rounded-lg">
+        <img
+          className="mb-5 w-40"
+          src={companyLogo}
+         
+          alt=""
+        />
+        <h3 className="text-xl text-gray-700 font-bold">
+          {jobTitle}
+        </h3>
+        <p className="text-gray-500 font-bold mt-1 mb-3">{companyName}</p>
+        <button className="btn-custom me-3">{jobType}</button>{" "}
+        <button className="btn-custom">{jobDuration}</button>
+        <div className="mt-4 mb-5 text-gray-600">
+          <span className="me-5">
+            {" "}
+            <img className="inline" src={LocationIcon} alt="" /> {location}
+          </span>
+
+          <span>
+            {" "}
+            <img className="inline" src={MoneyIcon} alt="" /> Salary : {salary}
+          </span>
+        </div>
+        <button className="btn-primary mb-3">View Details</button>
+      </div>
+  );
+};
+
+export default SingleJob;
