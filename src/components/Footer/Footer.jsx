bdc787dc5d046180cec971019664efe7dfca8861
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-12 22:04:41 +0600

    awesome custom footer added and finally project complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,77 +1,70 @@
 import React from "react";
-
+import FbIcon from '../../assets/social/fb.png';
+import TwIcon from '../../assets/social/fw.png';
+import InIcon from '../../assets/social/ins.png';
 const Footer = () => {
   return (
     <footer className="bg-black mt-40">
-      <div className="mt-40 container">
-        <footer className="footer p-10 text-white ">
-          <div>
-            <span className="footer-title">Services</span>
-            <a className="link link-hover">Branding</a>
-            <a className="link link-hover">Design</a>
-            <a className="link link-hover">Marketing</a>
-            <a className="link link-hover">Advertisement</a>
+
+      <div className="container py-24">
+       <div className="grid grid-cols-1 md:grid-cols-6 gap-8 text-gray-400">
+
+        <div className="col-span-2">
+          <h2 className="text-gray-200 font-bold text-2xl tracking-wider mb-6">CareerMaker</h2>
+          <p className="lg:w-3/4 mb-3">We are <strong>CareerMaker</strong> the company that always try to provide better job opportunity to our clients. To stay updated please subscribe to our newsletter.</p>
+          <div className="mb-10">
+          <img className="px-4 py-3 inline-block me-3 bg-white rounded-full" src={FbIcon} alt="" />
+          <img className="px-3 py-3 inline-block me-3 bg-white rounded-full" src={TwIcon} alt="" />
+          <img className="px-3 py-3 inline-block  bg-white rounded-full" src={InIcon} alt="" />
           </div>
-          <div>
-            <span className="footer-title">Company</span>
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
-          </div>
-          <div>
-            <span className="footer-title">Legal</span>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
-            <a className="link link-hover">Cookie policy</a>
-          </div>
-        </footer>
-        <footer className="footer px-10 py-4 border-t text-white rounded-lg rounded-t-none">
-          <div className="items-center grid-flow-col">
-            <div className=""></div>
-            <p>
-              Career Maker Industries Ltd. <br />
-              Providing reliable Job since 2001
-            </p>
-          </div>
-          <div className="md:place-self-center md:justify-self-end">
-            <div className="grid grid-flow-col gap-4">
-              <a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                </svg>
-              </a>
-              <a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                </svg>
-              </a>
-              <a>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  className="fill-current"
-                >
-                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </footer>
+        </div>
+
+        <div className="">
+          <h3 className="text-gray-200 font-semibold mb-6">Company</h3>
+          <ul>
+            <li className="mb-2">About Us</li>
+             <li className="mb-2">Work</li>
+             <li className="mb-2">Latest News</li>
+             <li className="mb-2">Careers</li>
+          </ul>
+        </div>
+
+        <div className="">
+          <h3 className="text-gray-200 font-semibold mb-6">Product</h3>
+          <ul>
+            <li className="mb-2">Prototype</li>
+             <li className="mb-2">Plans & Pricing</li>
+             <li className="mb-2">Customers</li>
+             <li className="mb-2">Integrations</li>
+          </ul>
+        </div>
+
+        <div className="">
+          <h3 className="text-gray-200 font-semibold mb-6">Support</h3>
+          <ul>
+            <li className="mb-2">Help Desk</li>
+             <li className="mb-2">Sales</li>
+             <li className="mb-2">Become a Partner</li>
+             <li className="mb-2">Developers</li>
+          </ul>
+        </div>
+
+        <div className="">
+          <h3 className="text-gray-200 font-semibold mb-6">Contact</h3>
+          <ul>
+            <li className="mb-2">524 Broadway , NYC</li>
+             <li className="mb-2">+1 777 - 978 - 5570</li>
+            
+          </ul>
+        </div>
+
+       </div>
+       <hr className="border-gray-600 mb-5 lg:mb-10" />
+
+       <div className="flex items-center justify-between">
+        <p className="text-gray-600">@2023 CareerMaker. All Rights Reserved</p>
+        <p className="text-gray-600">Powered by CareerMaker</p>
+       </div>
       </div>
     </footer>
   );
